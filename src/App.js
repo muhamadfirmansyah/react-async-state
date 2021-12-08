d48d79b5fc28173c23ce2be29d72521a91cae54f
Fnwrkio <firmansyah.assignment@gmail.com>
2021-12-08 10:11:45 +0700

    Asynchronous setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  // constructor() {
+  //   super()
+
+  //   this.state = {
+  //     increment: 47
+  //   }
+  // }
+
+  state = {
+    increment: 47
+  }
+
+  handleClick = () => {
+    // this.setState({ increment: this.state.increment + 1 })
+    // console.log(this.state.increment)
+
+    this.setState((prevState, prevProps) => {
+      return {
+        increment: prevState.increment + 1
+      }
+    }, () => {
+      console.log(this.state.increment)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            { this.state.increment }
+          </p>
+          <button
+            onClick={this.handleClick}>
+            Update Data
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
